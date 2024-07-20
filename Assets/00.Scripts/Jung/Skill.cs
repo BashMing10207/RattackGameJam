d407a1a0126c289f15aaef1c41bf5a066a7e3866
zhdltl601 <zhdltl601@gmail.com>
2024-07-20 11:49:16 +0900

    Add Card Manager

--- Assets/00.Scripts/Jung/Skill.cs
+++ Assets/00.Scripts/Jung/Skill.cs
@@ -4,10 +4,17 @@ using UnityEngine.UIElements;
 
 public abstract class Skill : MonoBehaviour
 {
+    [SerializeField] private SO_CardAsset so_Card;
+    public SO_CardAsset GetCardSO => so_Card;
+
     [SerializeField] protected float skillCoolDown;
     protected float _skillTimer;
     private List<ParticleSystem> particleSystems;
-    
+    protected virtual void Awake()
+    {
+        if(name == "_")
+            name = ToString();
+    }
     public virtual void TryActivateSkill()
     {
         if (skillCoolDown <= _skillTimer)
