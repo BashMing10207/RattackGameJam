b11ca473c3b33b495d4342f45df275c74a0fce1d
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-07-20 20:35:57 +0900

    Fix Fire Skill Name

--- Assets/00.Scripts/Skills/FireSkill.cs
+++ Assets/00.Scripts/Skills/FireSkill.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireGaySkill : Skill
+public class FireSkill : Skill
 {
     protected override void ActivateSkill(NetPlayerStone netPlayerStone)
     {
